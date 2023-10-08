0abb8b7d30fe9f53155231dfd16dfb4734db2c64
CaioXTSY <caio.teixeira@arapiraca.ufal.br>
2023-10-08 00:23:50 -0300

    redesign, ot

--- src/script.js
+++ src/script.js
@@ -1,18 +1,24 @@
-document.querySelectorAll('.sect, .video-sect, .book-sect').forEach(item => {
-  item.addEventListener('click', () => {
-      const content = item.nextElementSibling;
-      content.style.maxHeight = content.style.maxHeight ? null : content.scrollHeight + 'px';
-      item.querySelector('i').classList.toggle('fa-chevron-up');
-  });
-});
+document.querySelectorAll(".sect, .video-sect, .book-sect").forEach((item) => {
+  item.addEventListener("click", () => {
+    const content = item.nextElementSibling
+    const arrow = item.querySelector("i")
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null
+      content.style.marginBottom = 0
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px"
+      content.style.marginBottom = "1rem"
+    }
+    arrow.classList.toggle("rotate")
+  })
+})
 
-document.querySelectorAll('nav a').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
-      });
-  });
-});
+document.querySelectorAll("nav a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault()
 
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    })
+  })
+})
